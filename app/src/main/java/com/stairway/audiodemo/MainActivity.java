355a079010af3eb6989d17bcda3f0cffcdbb06c7
prabalchhatkuli <pchhatku@ramapo.edu>
2019-04-19 17:23:16 -0400

    final version plays jingle plays

--- app/src/main/java/com/stairway/audiodemo/MainActivity.java
+++ app/src/main/java/com/stairway/audiodemo/MainActivity.java
@@ -1,30 +1,35 @@
 package com.stairway.audiodemo;
 
+import android.app.Activity;
 import android.content.Context;
-import android.media.AudioManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
-import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.media.AudioManager;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     MediaPlayer mplayer;
     AudioManager audioManager;
 
-    public void playAudio(View view)
-    {
+    public void playAudio(View view) {
+
         mplayer.start();
+
     }
-    public void pauseAudio(View view)
-    {
+
+    public void pauseAudio(View view) {
+
         mplayer.pause();
+
     }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,26 +41,47 @@ public class MainActivity extends AppCompatActivity {
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
-
-        SeekBar volumeControl =(SeekBar) findViewById(R.id.seekBar);
-        final SeekBar timeControl =(SeekBar) findViewById(R.id.timeSeek);
-
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
         volumeControl.setMax(maxVolume);
         volumeControl.setProgress(curVolume);
 
-        timeControl.setMax(mplayer.getDuration());
+        volumeControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+        });
+
+
+        final SeekBar scrubber = (SeekBar) findViewById(R.id.timeSeek);
+        scrubber.setMax(mplayer.getDuration());
 
         new Timer().scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                timeControl.setProgress(mplayer.getCurrentPosition());
+                scrubber.setProgress(mplayer.getCurrentPosition());
+            }
+        },0, 100);
 
-            }},0, 10);
 
-        timeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        scrubber.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (fromUser) {
                 mplayer.seekTo(progress);
+            }
+
             }
 
             @Override
@@ -66,25 +92,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 mplayer.start();
-            }
-        });
-
-        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("SeekBar value", Integer.toString(progress));
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
     }
