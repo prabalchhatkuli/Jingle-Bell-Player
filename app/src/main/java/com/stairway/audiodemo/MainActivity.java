39fa9aca271d7cdccac8950d94d0cca008335c5d
prabalchhatkuli <pchhatku@ramapo.edu>
2019-04-19 16:38:42 -0400

    after adding the play bar

--- app/src/main/java/com/stairway/audiodemo/MainActivity.java
+++ app/src/main/java/com/stairway/audiodemo/MainActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mplayer;
@@ -27,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mplayer = MediaPlayer.create(this, R.raw.dogbark);
+        mplayer = MediaPlayer.create(this, R.raw.jingle);
 
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
@@ -35,28 +38,34 @@ public class MainActivity extends AppCompatActivity {
 
 
         SeekBar volumeControl =(SeekBar) findViewById(R.id.seekBar);
-        SeekBar timeControl =(SeekBar) findViewById(R.id.timeSeek);
+        final SeekBar timeControl =(SeekBar) findViewById(R.id.timeSeek);
 
         volumeControl.setMax(maxVolume);
         volumeControl.setProgress(curVolume);
 
-        timeControl.setMax();
-        timeControl.setProgress();
+        timeControl.setMax(mplayer.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timeControl.setProgress(mplayer.getCurrentPosition());
+
+            }},0, 10);
 
         timeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                mplayer.seekTo(progress);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                mplayer.pause();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                mplayer.start();
             }
         });
 
